dabe336e1b2b062d806129ec2dd5917c7b87a382
Samuel Leite <samuelleite.fl@gmail.com>
2024-05-09 19:29:56 -0400

    added function to toggle dark and light mode

---
+++ assets/js/logic.js
@@ -0,0 +1,16 @@
+// Access toggle switch HTML element
+// const themeSwitcher = document.querySelector ('#theme-switcher');
+
+const themeSwitcher = document.querySelector('#theme-switcher');
+
+const container = document.querySelector('.container');
+
+let  themeMode = 'dark'; 
+
+const toggleTheme = () => {
+  themeMode = themeMode === 'dark' ? 'light' : 'dark'
+  container.setAttribute("class", `container ${themeMode}`)
+}
+
+themeSwitcher.addEventListener("click", toggleTheme)
+
